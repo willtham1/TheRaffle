954e6a253c9d789aa855c848c680056e21d69b49
VLuu808 <vietbluu12@gmail.com>
2024-01-06 00:18:32 -0800

    Able to read csv file and check for duplicate names

--- TheRaffle/Program.cs
+++ TheRaffle/Program.cs
@@ -1,6 +1,9 @@
 // Were going to do a CLI first (Command Line Interface) once this works fully well go into a gui
 ////TODO Before running make sure you run dotnet restore to install packages
 
+using CsvHelper;
+using System.Drawing;
+using System.Globalization;
 do
 {
     Console.WriteLine("-----------------------------");
@@ -9,11 +12,50 @@ do
     Console.WriteLine("-----------------------------\n");
     // What the ? means it'll return null google null-conditional operators for c#
     string? menuOption = Console.ReadLine();
+    string? file = Console.ReadLine();
+    using var reader = new StreamReader(file);
+
     switch (menuOption)
     {
         case "1":
-            Console.WriteLine("This option does something: ");
-            Console.Read();
+            Console.WriteLine("This insert file: ");
+            cleanFile(reader);
+
+
             break;
     }
 } while (true);
+
+void cleanFile(StreamReader file)
+{
+    Console.WriteLine(file);
+    using (var csv = new CsvReader(file, CultureInfo.InvariantCulture))
+    {
+        var records = csv.GetRecords<getHeader>().ToList();
+        List<getHeader> Tickets = new List<getHeader>();
+        int counter = records.Count;
+        int index = 0;
+        while (counter != 0)
+        {
+            bool TrueOrFalse = true;
+
+            if (Tickets.Count > 0)
+            {
+                foreach (getHeader item in Tickets)
+                {
+                    if (item.Name.Equals(records[index].Name))
+                    {
+                        TrueOrFalse = false;
+                    }
+                }
+            }
+            if (TrueOrFalse == true)
+            {
+                Tickets.Add(records[index]);
+            }
+            index++;
+            counter--;
+        }
+    }
+}
+
---
+++ TheRaffle/getHeader.cs
@@ -0,0 +1,40 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+public class getHeader
+{
+    [Name("Title")]
+    public string Title { get; set; }
+    
+    [Name("Source")]
+
+    public string Source { get; set; }
+
+    [Name("Response Date")]
+
+    public string Response { get; set; }
+
+    [Name("1_Name")]
+    public string Name { get; set; }
+
+    [Name("1_Full Name")]
+    public string Full_Name { get; set; }
+
+    [Name("2_Email")]
+
+    public string Email { get; set; }
+
+
+    [Name("3_Size")]
+
+    public string Size { get; set; }
+    [Name("4_Location")]
+
+    public string Location { get; set; }
+    [Name("Consent")]
+
+    public string Consent { get; set; }
+    [Name("IP Address")]
+
+    public string IP_Adress { get; set; }
+}
+
